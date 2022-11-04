a8bd3b2ffa629935f6dc998bbf646230c3a4230a
Родион <rysaeva96@mail.ru>
2022-11-04 19:11:52 +0300

    3 задача

---
+++ src/ThirdTask.java
@@ -0,0 +1,16 @@
+class ThirdTask {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
